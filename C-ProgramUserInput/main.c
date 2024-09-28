537d797693970dc30428f40989d56fb18bf2026e
Cory Hodge <coryhodge21@gmail.com>
2024-09-27 22:48:51 -0400

    adding help info, prep to parse out flag

--- C-ProgramUserInput/main.c
+++ C-ProgramUserInput/main.c
@@ -15,33 +15,62 @@
 // Defines / Constants
 #define FLAG_PREFIX_INDICATOR "--"
 
+// Forward Decleration of Help Function
+void printHelp(void);
+
 /*     PROGRAM BEGIN    */
 int main(int argc, char ** argv)
 {
-    // printf("*********\n");
-    // printf("* FLAGS *\n");
-    // printf("--Flag=Value\n");
-    // printf("*********\n");
-    // printf("\n");
 
-    // Check if any arguments passed
+    // If no Argumkents Passed (only exec name in argv),
+    //  print out help info to user
+    if(argc == 1)
+    {
+        printHelp();
+
+        return 1;
+    }
+
+    // for arguments passed
     for(int inputs = 0; inputs < argc; inputs++)
     {
         // skip program executable name
         if(inputs == 0)
         {
-            
+            // display prog name
+            printf("Exec %s with flags :\n", argv[inputs]);
             continue;
         }
 
         printf("argv[%d]=%s\n",inputs, argv[inputs]);
 
         // If flag prefix found, parse flag,value pair
-        if( strncmp(FLAG_PREFIX_INDICATOR, argv[inputs], 2) == 0)
+        if( strncmp(FLAG_PREFIX_INDICATOR, argv[inputs], 2) == 0 )
         {
             printf("Flag Prefix Indicator Found\n");
+
+            // Parse Out Flag
+            
+
         }
 
     }
+
+    return 0;
 };
+
+//
+void printHelp(void)
+{
+    printf("*********\n");
+    printf("* FLAGS *\n");
+    printf("--Flag=Value\n");
+    printf("--Exec=A : Execute Process A\n");
+    printf("--Exec=B : Execute Process B\n");
+    printf("--DEBUG=A : Set Debug Flag A\n");
+    printf("--DEBUG=B : Set Debug Flag B\n");
+    printf("*********\n");
+    printf("\n");
+}
+
 #endif // _USER_INPUT_
